2b28f794d8ff10016433302f3b69507c91f407da
robert <robert.oerdoeg@gmail.com>
2017-07-05 17:36:46 +0200

    PlayerSelectionActivity

---
+++ app/src/main/java/com/example/rober/pubcrawlchallengeapp/PlayerSelectionActivity.java
@@ -0,0 +1,18 @@
+package com.example.rober.pubcrawlchallengeapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by rober on 05.07.2017.
+ */
+
+public class PlayerSelectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_playerselection);
+    }
+}
