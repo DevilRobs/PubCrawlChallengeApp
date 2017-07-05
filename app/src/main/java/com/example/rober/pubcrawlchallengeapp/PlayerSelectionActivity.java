e16f2532cd70006f28b8ee3b52456aa0a73f6f8a
robert <robert.oerdoeg@gmail.com>
2017-07-05 18:55:17 +0200

    PlayerSelection

--- app/src/main/java/com/example/rober/pubcrawlchallengeapp/PlayerSelectionActivity.java
+++ app/src/main/java/com/example/rober/pubcrawlchallengeapp/PlayerSelectionActivity.java
@@ -1,8 +1,15 @@
 package com.example.rober.pubcrawlchallengeapp;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 /**
  * Created by rober on 05.07.2017.
@@ -10,9 +17,32 @@ import android.support.v7.app.AppCompatActivity;
 
 public class PlayerSelectionActivity extends AppCompatActivity {
 
+    private Activity activity = this;
+    private String TAG = "PlayerSelection";
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_playerselection);
+        addPlayer();
+    }
+
+    public void addPlayer(){
+        System.out.println("test");
+        Log.d(TAG, "aufgerufen");
+        Button b = (Button) findViewById(R.id.b_addPlayer);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "clicked");
+                ActionBar.LayoutParams lparams = new ActionBar.LayoutParams(
+                        ActionBar.LayoutParams.WRAP_CONTENT, ActionBar.LayoutParams.WRAP_CONTENT);
+                TextView tv=new TextView(activity);
+                tv.setLayoutParams(lparams);
+                tv.setText("test");
+                LinearLayout lv = (LinearLayout) findViewById(R.id.layoutlinear);
+                lv.addView(tv);
+            }
+        });
     }
 }
