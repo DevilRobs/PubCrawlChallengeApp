477e0f78c4fa348981d9cfe6a54a95520a40f17b
robert <robert.oerdoeg@gmail.com>
2017-07-06 22:04:17 +0200

    PlayerSelection: - Added functionality to add more player Todo: - That switch changes between girl and boy - Design fixes

--- app/src/main/java/com/example/rober/pubcrawlchallengeapp/PlayerSelectionActivity.java
+++ app/src/main/java/com/example/rober/pubcrawlchallengeapp/PlayerSelectionActivity.java
@@ -7,8 +7,11 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Switch;
 import android.widget.TextView;
 
 /**
@@ -19,30 +22,69 @@ public class PlayerSelectionActivity extends AppCompatActivity {
 
     private Activity activity = this;
     private String TAG = "PlayerSelection";
+    private int playerCounter = 4;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_playerselection);
-        addPlayer();
+
+        //Initialize Elements
+        Button btn_addPlayer = (Button) findViewById(R.id.btn_addPlayer);
+
+        //Register the Listener
+        btn_addPlayer.setOnClickListener(addPlayer);
+
     }
 
-    public void addPlayer(){
-        System.out.println("test");
-        Log.i(TAG, "aufgerufen");
-        Button b = (Button) findViewById(R.id.b_addPlayer);
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.i(TAG, "clicked");
-                ActionBar.LayoutParams lparams = new ActionBar.LayoutParams(
-                        ActionBar.LayoutParams.WRAP_CONTENT, ActionBar.LayoutParams.WRAP_CONTENT);
-                TextView tv=new TextView(activity);
-                tv.setLayoutParams(lparams);
-                tv.setText("test");
-                LinearLayout lv = (LinearLayout) findViewById(R.id.layoutlinear);
-                lv.addView(tv);
-            }
-        });
-    }
+    private View.OnClickListener addPlayer = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Log.i(TAG, "aufgerufen");
+
+            //Initalize Parameter for the layout
+            LinearLayout.LayoutParams ll_params = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+            );
+            //Initialize Paramter for the edit text
+            LinearLayout.LayoutParams et_params = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT, 7
+
+            );
+            //Initialize Paramter for the switch
+            LinearLayout.LayoutParams sw_params = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT, 2
+            );
+
+            //create a new LinearLayout
+            LinearLayout ll_player = new LinearLayout(activity);
+            ll_player.setLayoutParams(ll_params);
+            ll_player.setOrientation(LinearLayout.HORIZONTAL);
+            ll_player.setPadding(10,10,10,10);
+            ll_player.setWeightSum(10);
+
+
+            //create a new EditText
+            EditText et_playerName = new EditText(activity);
+            et_playerName.setLayoutParams(et_params);
+            playerCounter += 1;
+            et_playerName.setHint("Player "+playerCounter);
+
+
+            //create a new Switch
+            Switch sw_playerGender = new Switch(activity);
+            sw_playerGender.setLayoutParams(sw_params);
+            sw_playerGender.setPadding(15,0,0,0);
+            sw_playerGender.setText(R.string.sw_gender_male);
+
+            //register elements
+            ll_player.addView(et_playerName);
+            ll_player.addView(sw_playerGender);
+            LinearLayout lv = (LinearLayout) findViewById(R.id.layout_player);
+            lv.addView(ll_player);
+        }
+    };
 }
