e570759e49d6f95521a351c853a0fc5a62bbd005
Danielks92 <daniel@koerner-schenk.at>
2017-07-10 22:54:10 +0200

    mein unglaublicher commit2

--- app/src/main/java/com/example/rober/pubcrawlchallengeapp/MainActivity.java
+++ app/src/main/java/com/example/rober/pubcrawlchallengeapp/MainActivity.java
@@ -10,11 +10,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -46,15 +54,33 @@ public class MainActivity extends Activity {
         tv_appname.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               startActivity(new Intent(mainActivity, PlayerSelectionActivity.class));
+                startActivity(new Intent(mainActivity, PlayerSelectionActivity.class));
             }
         });
         challengeListSetup();
         handleStyleChange();
+        setupListListener();
+        getAllChallengesFromDatabase();
 
+    }
 
+    private void setupListListener() {
 
+        challengesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Challenges c = (Challenges)challengesListView.getItemAtPosition(position);
+                Toast.makeText(mainActivity, c.getChallengeName(), Toast.LENGTH_LONG).show();
+            }
+        });
 
+        challengesListView_twoCol.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Challenges c = (Challenges)challengesListView_twoCol.getItemAtPosition(position);
+                Toast.makeText(mainActivity, c.getChallengeName(), Toast.LENGTH_LONG).show();
+            }
+        });
 
 
     }
@@ -66,7 +92,7 @@ public class MainActivity extends Activity {
         challengesListView = (ListView) findViewById(R.id.lv_challengesList_1col);
 
         // create the List with the challenges to the list view - just for testing
-        List <Challenges> challengesList = new LinkedList<>();
+        List<Challenges> challengesList = new LinkedList<>();
         challengesList.add(new Challenges("Erste Challenge",
                 "1",
                 R.drawable.ic_android_black_24dp));
@@ -79,41 +105,38 @@ public class MainActivity extends Activity {
                 "3", R.drawable.ic_thumb_up_black_24dp));
 
 
-            challengesListView_twoCol = (ListView) findViewById(R.id.lv_challengesList_2col);
+        challengesListView_twoCol = (ListView) findViewById(R.id.lv_challengesList_2col);
 
-            col1 = new LinkedList<>();
-            col2 = new LinkedList<>();
+        col1 = new LinkedList<>();
+        col2 = new LinkedList<>();
 
-            int numberOfItems = challengesList.size()%2 == 0 ? challengesList.size()/2 : challengesList.size()/2+1;
+        int numberOfItems = challengesList.size() % 2 == 0 ? challengesList.size() / 2 : challengesList.size() / 2 + 1;
 
-            Log.d("mod", "" + numberOfItems%2);
+        Log.d("mod", "" + numberOfItems % 2);
 
-            for(int i = 0; i < numberOfItems; i++){
-                col1.add(challengesList.get(i));
+        for (int i = 0; i < numberOfItems; i++) {
+            col1.add(challengesList.get(i));
 
-            }
-            for(int j = numberOfItems; j < challengesList.size(); j++){
-                col2.add(challengesList.get(j));
-            }
+        }
+        for (int j = numberOfItems; j < challengesList.size(); j++) {
+            col2.add(challengesList.get(j));
+        }
 
-            for(Challenges c : col1){
-                Log.d("listed1", c.getChallengeName());
-            }
-            for(Challenges c : col2){
-                Log.d("listed2", c.getChallengeName());
-            }
-
-
+        for (Challenges c : col1) {
+            Log.d("listed1", c.getChallengeName());
+        }
+        for (Challenges c : col2) {
+            Log.d("listed2", c.getChallengeName());
+        }
 
 
+        // set the adapter of the listview with the custom layout + adapter
+        challengesListAdapter = new ChallengesListViewAdapter(this,
+                R.layout.challenge_list_item, challengesList);
 
-            // set the adapter of the listview with the custom layout + adapter
-            challengesListAdapter = new ChallengesListViewAdapter(this,
-                    R.layout.challenge_list_item, challengesList);
-
-            // set the adapter to show the content in the listview
-            challengesListView.setAdapter(challengesListAdapter);
-            challengesListView.setVisibility(View.VISIBLE);
+        // set the adapter to show the content in the listview
+        challengesListView.setAdapter(challengesListAdapter);
+        challengesListView.setVisibility(View.VISIBLE);
 
 
     }
@@ -129,7 +152,6 @@ public class MainActivity extends Activity {
                 selectStyleDialog.setContentView(R.layout.dialog_select_style);
                 selectStyleDialog.setTitle("Custom Dialog");
                 selectStyleDialog.show();
-
 
 
                 Button btn_full = (Button) selectStyleDialog.findViewById(R.id.btn_styleFull);
@@ -187,13 +209,19 @@ public class MainActivity extends Activity {
                     }
                 });
 
-
-
-
-
             }
         });
 
     }
 
+    public void getAllChallengesFromDatabase() {
+        // Read from the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("Challenges");
+
+
+
+
+
+    }
 }
