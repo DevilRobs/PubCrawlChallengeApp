27d5379fe4cab911c3e355630bfee61549998b88
DevilRobs <robert.oerdoeg@gmail.com>
2017-08-02 22:46:15 +0200

    Popup for scores.. not finished

--- app/src/main/java/com/example/rober/pubcrawlchallengeapp/ChallengeActivity.java
+++ app/src/main/java/com/example/rober/pubcrawlchallengeapp/ChallengeActivity.java
@@ -7,8 +7,11 @@ import android.os.CountDownTimer;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
 import java.util.HashMap;
@@ -25,6 +28,8 @@ import utilities.ProducedChallenge;
 
 public class ChallengeActivity extends AppCompatActivity {
 
+    LinearLayout ll_challengeActivity;
+
     private static int currentTaskId = -1;
     private String currentPlayer;
 
@@ -32,7 +37,7 @@ public class ChallengeActivity extends AppCompatActivity {
 
     TextView tv_task, tv_player, tv_points;
 
-    Button btn_done, btn_refuse;
+    Button btn_done, btn_refuse, btn_score, btn_pause;
 
     List<String> tasks;
     List<Integer> points;
@@ -42,10 +47,15 @@ public class ChallengeActivity extends AppCompatActivity {
 
     final private long DIALOG_DISMISS_TIMER = 3000;
 
+    PopupWindow popup_scoreboard;
+    Boolean popup_scoreboard_isClicked = true;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_challenge);
+
+        ll_challengeActivity = (LinearLayout) findViewById(R.id.ll_challengeActivity);
 
         tv_task = (TextView) findViewById(R.id.tv_task);
         tv_player = (TextView) findViewById(R.id.tv_player);
@@ -53,15 +63,18 @@ public class ChallengeActivity extends AppCompatActivity {
 
         btn_done = (Button) findViewById(R.id.btn_done);
         btn_refuse = (Button) findViewById(R.id.btn_refuse);
+        btn_score = (Button) findViewById(R.id.btn_scores);
+        btn_pause = (Button) findViewById(R.id.btn_pause);
 
         tasks = getIntent().getStringArrayListExtra("Tasks");
         points = getIntent().getIntegerArrayListExtra("Points");
         player = getIntent().getStringArrayListExtra("Players");
 
         playerMap = new HashMap<>();
-        for(String p : player){
-            playerMap.put(p,0);
+        for (String p : player) {
+            playerMap.put(p, 0);
         }
+
 
         //register listener
         btn_done.setOnClickListener(doneListener);
@@ -77,17 +90,18 @@ public class ChallengeActivity extends AppCompatActivity {
         public void onClick(View v) {
 
             String earnedPoints = String.valueOf(points.get(currentTaskId));
-            playerMap.put(currentPlayer, playerMap.get(currentPlayer)+points.get(currentTaskId));
+            playerMap.put(currentPlayer, playerMap.get(currentPlayer) + points.get(currentTaskId));
 
             //We create and show an AlertDialog (I WANT SOME FANCY ANIMATIONS!!!!!!!)
             AlertDialog.Builder builder = new AlertDialog.Builder(activity);
-            builder.setMessage(currentPlayer+" -> "+earnedPoints);
+            builder.setMessage(currentPlayer + " -> " + earnedPoints);
             final AlertDialog dialog = builder.create();
             dialog.show();
             new CountDownTimer(DIALOG_DISMISS_TIMER, 1000) {
                 @Override
                 public void onTick(long millisUntilFinished) {
                 }
+
                 @Override
                 public void onFinish() {
                     dialog.dismiss();
@@ -105,12 +119,27 @@ public class ChallengeActivity extends AppCompatActivity {
     };
 
 
-    private void setTask(){
+    private void setTask() {
         currentTaskId++;
         currentPlayer = player.get(new Random().nextInt(player.size()));
-        tv_player.setText(currentPlayer+": ");
+        tv_player.setText(currentPlayer + ": ");
         tv_task.setText(tasks.get(currentTaskId));
-        tv_points.setText("Reward: "+String.valueOf(points.get(currentTaskId))+" Points!");
+        tv_points.setText("Reward: " + String.valueOf(points.get(currentTaskId)) + " Points!");
     }
 
-}
+    private View.OnClickListener scoreboardListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if (popup_scoreboard_isClicked) {
+                popup_scoreboard_isClicked = false;
+                popup_scoreboard.showAtLocation(ll_challengeActivity, Gravity.BOTTOM, 10, 10);
+                popup_scoreboard.update(50, 50, 320, 90);
+            } else {
+                popup_scoreboard_isClicked = true;
+                popup_scoreboard.dismiss();
+            }
+        }
+    };
+};
+
+
